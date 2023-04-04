e06785d10ade532886262d1b9a0aa90dc8a0906b
Michal Murawski <michal.murawski@90poe.io>
2023-04-04 12:29:19 +0200

    basic scrapper server

---
+++ apps/scrapper/src/index.ts
@@ -0,0 +1,16 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript on Vercel");
+});
+
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
